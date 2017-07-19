5c029ac1f94f0ce13b46f0fce0a6e8b03e976cda
helmiriawan <helmiriawan@student.tudelft.nl>
2017-07-19 19:40:45 +0200

    Add visualization task

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,9 +3,12 @@
 #################
 
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 working_directory = "C:/Users/helmi/unix/CS4035/lab_python/lab1/"
 input_file = working_directory + "data_for_student_case.csv"
+output_file = working_directory + "test.png"
 
 
 
@@ -18,20 +21,15 @@ dataset.columns
 dataset.info()
 dataset.head()
 dataset.describe()
-
 dataset['simple_journal'].value_counts()
 
-# to be changed: bookingdate, issuercountrycode, txvariantcode, currencycode, 
-#                shoppercountrycode, shopperinteraction, simple_journal,
-#                cardverificationcodesupplied, cvcresponsecode, creationdate, 
-#                accountcode
 
 
 ####################
 # Data preparation #
 ####################
 
-# Remove some data
+# Clean the data
 dataset = dataset.dropna()
 dataset = dataset[dataset.simple_journal != "Refused"]
 
@@ -42,3 +40,30 @@ for column in ['bookingdate', 'creationdate']:
 for column in ['issuercountrycode', 'txvariantcode', 'currencycode', 'shoppercountrycode', 'shopperinteraction', 'simple_journal', 'cardverificationcodesupplied', 'cvcresponsecode', 'accountcode']:
     dataset[column] = dataset[column].astype('category')
 
+
+
+######################
+# Visualization task #
+######################
+
+# Heat map
+for flag in ['Chargeback', 'Settled']:
+    
+    # filter and aggregate data
+    subset = dataset[dataset['simple_journal'] == flag]
+    aggregation_data = subset.groupby(['shoppercountrycode', 'issuercountrycode']).size().reset_index(name='count')
+    
+    # filter country code for pretty visualization
+    issuer_country = list(dataset.issuercountrycode[dataset.simple_journal == 'Chargeback'].unique())
+    shopper_country = list(dataset.shoppercountrycode[dataset.simple_journal == 'Chargeback'].unique())
+    aggregation_data = aggregation_data[aggregation_data['issuercountrycode'].isin(issuer_country)]
+    aggregation_data = aggregation_data[aggregation_data['shoppercountrycode'].isin(shopper_country)]
+    
+    # generate pivot and heat map
+    pivot_data = aggregation_data.pivot(index='shoppercountrycode', columns='issuercountrycode', values='count')
+    sns.heatmap(pivot_data, cmap='viridis', linewidths=.5)
+    if flag == 'Chargeback' :
+        plt.title('Heat Map of Issuer and Shopper Country Code from Fraudulent Transaction')
+    else :
+        plt.title('Heat Map of Issuer and Shopper Country Code from Legitimate Transaction')
+    plt.show()
