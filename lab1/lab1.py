2722b86b59d7fbda3fd9e54b2a52ac89fa50f3e8
helmiriawan <helmiriawan@student.tudelft.nl>
2017-07-21 19:24:27 +0200

    Add box plot

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -38,6 +38,10 @@ dataset['simple_journal'].value_counts()
 dataset = dataset.dropna()
 dataset = dataset[dataset.simple_journal != "Refused"]
 
+# Change the label (simple_journal)
+dataset.simple_journal[dataset['simple_journal'] == 'Chargeback'] = 'Fraudulent'
+dataset.simple_journal[dataset['simple_journal'] == 'Settled'] = 'Legitimate'
+
 # Change data type
 for column in ['bookingdate', 'creationdate']:
     dataset[column] = pd.to_datetime(dataset.bookingdate, format='%Y-%m-%d', errors='coerce')
@@ -52,25 +56,36 @@ for column in ['issuercountrycode', 'txvariantcode', 'currencycode', 'shoppercou
 ######################
 
 # Heat map
-for flag in ['Chargeback', 'Settled']:
+for flag in ['Fraudulent', 'Legitimate']:
     
     # filter and aggregate data
     subset = dataset[dataset['simple_journal'] == flag]
     aggregation_data = subset.groupby(['shoppercountrycode', 'issuercountrycode']).size().reset_index(name='count')
     
     # filter country code for pretty visualization
-    issuer_country = list(dataset.issuercountrycode[dataset.simple_journal == 'Chargeback'].unique())
-    shopper_country = list(dataset.shoppercountrycode[dataset.simple_journal == 'Chargeback'].unique())
+    issuer_country = list(dataset.issuercountrycode[dataset.simple_journal == 'Fraudulent'].unique())
+    shopper_country = list(dataset.shoppercountrycode[dataset.simple_journal == 'Fraudulent'].unique())
     aggregation_data = aggregation_data[aggregation_data['issuercountrycode'].isin(issuer_country)]
     aggregation_data = aggregation_data[aggregation_data['shoppercountrycode'].isin(shopper_country)]
     
     # generate pivot and heat map
     pivot_data = aggregation_data.pivot(index='shoppercountrycode', columns='issuercountrycode', values='count')
     sns.heatmap(pivot_data, cmap='viridis', linewidths=.5)
-    if flag == 'Chargeback' :
-        plt.title('Heat Map of Issuer and Shopper Country Code from Fraudulent Transaction')
+    plt.xlabel('Issuer Country Code')
+    plt.ylabel('Shopper Country Code')
+    if flag == 'Fraudulent' :
+        plt.title('Issuer-Shopper Country Code of Fraudulent Transactions')
         plt.savefig(figure_directory + 'heatmap_fraudulent.png')
     else :
-        plt.title('Heat Map of Issuer and Shopper Country Code from Legitimate Transaction')
+        plt.title('Issuer-Shopper Country Code of Legitimate Transactions')
         plt.savefig(figure_directory + 'heatmap_legitimate.png')
     plt.show()
+
+# Box plot
+sns.boxplot(x="simple_journal", y="amount", data=dataset[dataset['amount'] <= 300000])   # filter amount for pretty visualization
+plt.title('Amount Distribution of Fraudulent and Legitimate Transactions')
+plt.xlabel('')
+plt.ylabel('Amount')
+plt.show()
+
+
