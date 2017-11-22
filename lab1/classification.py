3e329f9d2d5b0156d2303d7416c7d51293b728fb
helmiriawan <helmiriawan@student.tudelft.nl>
2017-11-22 18:00:40 +0100

    Move functions for classification task to another file

---
+++ lab1/classification.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+
+from imblearn.over_sampling import SMOTE
+
+
+# Create function to run cross validation
+def cross_validation(method, variable, flag, number_of_fold):
+
+    # Initiate the K-fold
+    k_fold = KFold(n_splits=number_of_fold, shuffle=True, random_state=42)
+
+    # Initiate the variables
+    all_true_positives = []
+    all_false_positives = []
+    all_true_negatives = []
+    all_false_negatives = []
+    all_auc = []
+
+    for train_index, test_index in k_fold.split(variable):
+
+        # Split train and test data
+        variable_train, variable_test = variable.iloc[train_index], variable.iloc[test_index]
+        flag_train, flag_test = flag.iloc[train_index], flag.iloc[test_index]
+
+        # Apply SMOTE
+        oversampling = SMOTE(ratio=float(0.18), random_state=42)
+        variable_oversampling, flag_oversampling = oversampling.fit_sample(variable_train, flag_train)
+
+        # Train classifier
+#       method.fit(variable_train, flag_train)
+        method.fit(variable_oversampling, flag_oversampling)
+
+        # Evaluate the model
+        flag_prediction = method.predict(variable_test)
+        table_of_confusion = confusion_matrix(flag_test, flag_prediction, labels=[1, 0])
+
+        true_positives = table_of_confusion[0][0]
+        false_positives = table_of_confusion[1][0]
+        true_negatives = table_of_confusion[1][1]
+        false_negatives = table_of_confusion[0][1]
+
+        flag_prediction_probability = method.predict_proba(variable_test)[:, 1]
+        area_under_curve = roc_auc_score(flag_test, flag_prediction_probability)
+
+        # Put the evaluation result to the list
+        all_true_positives.append(true_positives)
+        all_false_positives.append(false_positives)
+        all_true_negatives.append(true_negatives)
+        all_false_negatives.append(false_negatives)
+        all_auc.append(area_under_curve)
+
+    # Change the list to numpy array
+    all_true_positives = np.array(all_true_positives)
+    all_false_positives = np.array(all_false_positives)
+    all_true_negatives = np.array(all_true_negatives)
+    all_false_negatives = np.array(all_false_negatives)
+    all_auc = np.array(all_auc)
+
+    return all_true_positives, all_false_positives, all_true_negatives, all_false_negatives, all_auc
+
+
+# Create function to show the evaluation result from cross validation
+def evaluation_result(true_positives, false_positives, true_negatives, false_negatives, auc):
+
+    accuracy = (true_positives+true_negatives) / (true_positives+false_positives+true_negatives+false_negatives)
+    sensitivity = true_positives / (true_positives+false_negatives)
+    specificity = true_negatives / (false_positives+true_negatives)
+    precision = true_positives / (true_positives+false_positives)
+    f_measure = 2 * precision * sensitivity / (precision+sensitivity)
+
+    print("True positives: {}".format(np.mean(true_positives)))
+    print("False positives: {}".format(np.mean(false_positives)))
+    print("True negatives: {}".format(np.mean(true_negatives)))
+    print("False negatives: {}".format(np.mean(false_negatives)))
+
+    print("Accuracy: {}".format(np.mean(accuracy)))
+    print("Sensitivity: {}".format(np.mean(sensitivity)))
+    print("Specificity: {}".format(np.mean(specificity)))
+    print("Precision: {}".format(np.mean(precision)))
+    print("F-measure: {}".format(np.mean(f_measure)))
+    print("AUC: {}".format(np.mean(auc)))
--- lab1/main.py
+++ lab1/main.py
@@ -3,19 +3,15 @@
 #################
 
 import os
-import numpy as np
 import pandas as pd
 
 import visualization
 import imbalance
+import classification
 
 from sklearn import tree
 
-from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
-
-from sklearn.metrics import roc_auc_score
-from sklearn.metrics import confusion_matrix
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
@@ -141,84 +137,6 @@ feature = subset.drop('simple_journal', axis=1)
 feature = pd.get_dummies(feature)
 
 
-# Create function to run cross validation
-def cross_validation(method, variable, flag, number_of_fold):
-
-    # Initiate the K-fold
-    k_fold = KFold(n_splits=number_of_fold, shuffle=True, random_state=42)
-
-    # Initiate the variables
-    all_true_positives = []
-    all_false_positives = []
-    all_true_negatives = []
-    all_false_negatives = []
-    all_auc = []
-
-    for train_index, test_index in k_fold.split(variable):
-
-        # Split train and test data
-        variable_train, variable_test = variable.iloc[train_index], variable.iloc[test_index]
-        flag_train, flag_test = flag.iloc[train_index], flag.iloc[test_index]
-
-        # Apply SMOTE
-        resample = SMOTE(ratio=float(0.18), random_state=42)
-        variable_resample, flag_resample = resample.fit_sample(variable_train, flag_train)
-
-        # Train classifier
-#       method.fit(variable_train, flag_train)
-        method.fit(variable_resample, flag_resample)
-
-        # Evaluate the model
-        flag_prediction = method.predict(variable_test)
-        table_of_confusion = confusion_matrix(flag_test, flag_prediction, labels=[1, 0])
-
-        true_positives = table_of_confusion[0][0]
-        false_positives = table_of_confusion[1][0]
-        true_negatives = table_of_confusion[1][1]
-        false_negatives = table_of_confusion[0][1]
-
-        flag_prediction_probability = method.predict_proba(variable_test)[:, 1]
-        area_under_curve = roc_auc_score(flag_test, flag_prediction_probability)
-
-        # Put the evaluation result to the list
-        all_true_positives.append(true_positives)
-        all_false_positives.append(false_positives)
-        all_true_negatives.append(true_negatives)
-        all_false_negatives.append(false_negatives)
-        all_auc.append(area_under_curve)
-
-    # Change the list to numpy array
-    all_true_positives = np.array(all_true_positives)
-    all_false_positives = np.array(all_false_positives)
-    all_true_negatives = np.array(all_true_negatives)
-    all_false_negatives = np.array(all_false_negatives)
-    all_auc = np.array(all_auc)
-
-    return all_true_positives, all_false_positives, all_true_negatives, all_false_negatives, all_auc
-
-
-# Create function to show the evaluation result from cross validation
-def evaluation_result(true_positives, false_positives, true_negatives, false_negatives):
-
-    accuracy = (true_positives+true_negatives) / (true_positives+false_positives+true_negatives+false_negatives)
-    sensitivity = true_positives / (true_positives+false_negatives)
-    specificity = true_negatives / (false_positives+true_negatives)
-    precision = true_positives / (true_positives+false_positives)
-    f_measure = 2 * precision * sensitivity / (precision+sensitivity)
-
-    print("True positives: {}".format(np.mean(true_positives)))
-    print("False positives: {}".format(np.mean(false_positives)))
-    print("True negatives: {}".format(np.mean(true_negatives)))
-    print("False negatives: {}".format(np.mean(false_negatives)))
-
-    print("Accuracy: {}".format(np.mean(accuracy)))
-    print("Sensitivity: {}".format(np.mean(sensitivity)))
-    print("Specificity: {}".format(np.mean(specificity)))
-    print("Precision: {}".format(np.mean(precision)))
-    print("F-measure: {}".format(np.mean(f_measure)))
-    print("AUC: {}".format(np.mean(auc)))
-
-
 # Set algorithm
 algorithm = LogisticRegression()
 
@@ -232,8 +150,8 @@ algorithm = tree.DecisionTreeClassifier()
 """
 
 # Evaluate the classifier with cross validation
-tp, fp, tn, fn, auc = cross_validation(algorithm, feature, label, 10)
-evaluation_result(tp, fp, tn, fn)
+tp, fp, tn, fn, auc = classification.cross_validation(algorithm, feature, label, 10)
+classification.evaluation_result(tp, fp, tn, fn, auc)
 
 
 # Visualize the white-box
